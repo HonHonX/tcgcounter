91e2ab2522d718175812bece5766dd6c5c1ba178
Sherry <119943082+HonHonX@users.noreply.github.com>
2025-01-22 23:40:14 +0100

    update paths

--- countdown_project/countdown/urls.py
+++ countdown_project/countdown/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/counter', views.counter_view, name='counter'),
+    path('counter', views.counter_view, name='counter'),
     path('admin_counter/', views.admin_counter_view, name='admin_counter'),
 ]
--- countdown_project/countdown_project/urls.py
+++ countdown_project/countdown_project/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from countdown.views import counter_view
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('counter', counter_view, name='counter'),
-    path('', counter_view, name='home'),
+    path('counter', include('countdown.urls')),  # Include the countdown app URLs
+    path('', include('countdown.urls')),  # Include the countdown app URLs for the home page
 ]
