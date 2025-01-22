c14dcdd24901c558a300183d1f6e11cd37c67cfa
Sherry <119943082+HonHonX@users.noreply.github.com>
2025-01-22 23:26:42 +0100

    update wsgi

--- countdown_project/countdown_project/wsgi.py
+++ countdown_project/countdown_project/wsgi.py
@@ -1,14 +1,17 @@
 import os
 import sys
 
-# Add the path to your project
-path = '/home/YugiohCounter/countdown_project'
-if path not in sys.path:
-    sys.path.append(path)
+# Add your project directory to the sys.path
+project_home = '/home/YugiohCounter/tcgcounter/countdown_project'
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
-# Set the Django settings module
+# Set the environment variable for Django settings
 os.environ['DJANGO_SETTINGS_MODULE'] = 'countdown_project.settings'
 
-# Import Django and start the application
+# Activate your virtual environment (if you created one)
+activate_env = os.path.expanduser("/home/YugiohCounter/venv/bin/activate_this.py")
+exec(open(activate_env).read(), {'__file__': activate_env})
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
